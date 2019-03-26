ab235014c2a8b9cf1391f9b7d5c5887068394431
Andy Fraussen <fraussen.andy@gmail.com>
2019-03-26 14:58:31 +0100

    reeks 2 af

--- jquery-exercises-2/Exercises_1/main.js
+++ jquery-exercises-2/Exercises_1/main.js
@@ -1,1 +1,4 @@
 // Write your solution here
+$( "#click-me" ).click(function() {
+  alert( "we only go backwards" );
+});
--- jquery-exercises-2/Exercises_7/main.js
+++ jquery-exercises-2/Exercises_7/main.js
@@ -1,22 +1,52 @@
 $(document).ready(function(){
     /* On click: Enlarge the font (120%) */
-    
-    
+    $('li:first-child').click(function(){
+      $('#content').css('font-size', '120%');
+    });
+
     /* On click: Decrease the font size (80%) */
+    $('li:nth-child(2)').click(function(){
+      $('#content').css('font-size', '80%');
+    });
 
-    
     /* On click: Bold or normalize the green words */
 
-    
+    $('li:nth-child(3)').click(function(){
+    var value = $(this).html();
+    if(value=='Bold'){
+        $(this).html('Normal');
+        $('.green').css('font-weight', 'bold');
+    }else{
+    $(this).html('Bold');
+    $('.green').css('font-weight', 'normal');
+    }
+})
     /* On click: Bold or normalize the green words */
 
-    
+    $('li:nth-child(4)').click(function(){
+    var value = $(this).html();
+    if(value=='Underline'){
+        $(this).html('Normal');
+        $('.red').css('text-decoration', 'underline');
+    }else{
+    $(this).html('Underline');
+    $('.red').css('text-decoration', 'none');
+    }
+})
+
     /* On click: Replace the logo with another image */
-    
-    
+
+  $('img').click(function(){
+    $('img').attr('src',"https://i.ytimg.com/vi/mSnMTMlJzME/maxresdefault.jpg");
+  });
+
     /* On hover: Display the URL of links in a tooltip when hovering over links */
-    
-    
+
+    $('a').mouseover(function(){
+      $(this).attr('title', $(this).attr('href'));
+    })
+
     /* On click: add "Chapter 1:" before the 1st title h2 and "Chapter 2:" before the 2nd title h2 */
-    
+      $('img').after('<h2>Chapter 1:</h2>');
+      $('p:nth-child(5)').after('<h2>Chapter 2:</h2>');
 });
